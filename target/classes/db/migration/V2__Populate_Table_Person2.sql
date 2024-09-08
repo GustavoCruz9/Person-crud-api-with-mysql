480a29cbbb2799123954e6d432ee98429bde97dd
Gustavo da Cruz <161187263+GustavoCruz9@users.noreply.github.com>
2024-09-08 00:21:24 -0300

    Migrations
    Implementação de Migrations usando o Flyway

---
+++ target/classes/db/migration/V2__Populate_Table_Person2.sql
@@ -0,0 +1,7 @@
+INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `last_name`) VALUES
+	(1, 'São Paulo', 'Ayrton', 'Male', 'Senna'),
+	(2, 'SP', 'Gustavo', 'Male', 'da Cruz'),
+	(3, 'Londres', 'Lewis', 'Male', 'Hamilton'),
+	(4, 'Hasselt', 'Max', 'Male', 'Verstapen'),
+	(5, 'Mvezo', 'Nelson', 'Male', 'Mandela'),
+	(6, 'Croacia', 'Nikola', 'Male', 'Tesla');
