b57a09658e8b9df961c1db7c359a266c05e41151
Gustavo da Cruz <161187263+GustavoCruz9@users.noreply.github.com>
2024-09-08 01:36:23 -0300

    Content Negotiation

---
+++ src/main/java/cruz/gustavo/config/WebConfig.java
@@ -0,0 +1,41 @@
+package cruz.gustavo.config;
+
+import java.util.List;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import cruz.gustavo.serialization.converter.YamlJackson2HttpMesageConverter;
+
+//classe de configuração
+@Configuration
+public class WebConfig implements WebMvcConfigurer{
+	
+	private static final MediaType MEDIA_TYPE_APPLICATION_YML = MediaType.valueOf("application/x-yml");
+
+	@Override
+	public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+		//via QUERY PARAM, ex.: http://localhost:8080/api/person/v1?mediaType=xml
+		/*
+		configurer.favorParameter(true).parameterName("mediaType").ignoreAcceptHeader(true).useRegisteredExtensionsOnly(false)
+					.defaultContentType(MediaType.APPLICATION_JSON)
+						.mediaType("json", MediaType.APPLICATION_JSON)
+						.mediaType("xml", MediaType.APPLICATION_XML);*/
+		
+		//via HEADER PARAM
+		configurer.favorParameter(false).ignoreAcceptHeader(false).useRegisteredExtensionsOnly(false)
+					.defaultContentType(MediaType.APPLICATION_JSON)
+						.mediaType("json", MediaType.APPLICATION_JSON)
+						.mediaType("xml", MediaType.APPLICATION_XML) //serializações suportadas
+						.mediaType("x-yaml", MEDIA_TYPE_APPLICATION_YML);
+	}
+	
+	@Override
+	public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+		converters.add(new YamlJackson2HttpMesageConverter());
+	}
+
+}
--- src/main/java/cruz/gustavo/controllers/PersonController.java
+++ src/main/java/cruz/gustavo/controllers/PersonController.java
@@ -3,7 +3,6 @@ package cruz.gustavo.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 import cruz.gustavo.data.vo.v1.PersonVO;
 import cruz.gustavo.data.vo.v2.PersonVOV2;
 import cruz.gustavo.services.PersonServices;
+import cruz.gustavo.util.MediaType;
 
 @RestController
 @RequestMapping("/api/person/v1")
@@ -25,25 +25,25 @@ public class PersonController {
 	@Autowired
 	private PersonServices service;
 
-	@GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+	@GetMapping(value = "/{id}", produces = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML})
 	public PersonVO findById(@PathVariable(value = "id") Long id) throws Exception {
 		return service.findById(id);
 	}
 	
-	@PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, 
-			produces = MediaType.APPLICATION_JSON_VALUE)
+	@PostMapping(consumes = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML} , 
+			produces = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML})
 	public PersonVO create(@RequestBody PersonVO PersonVO) throws Exception {
 		return service.create(PersonVO);
 	}
 	
-	@PostMapping(value = "/v2", consumes = MediaType.APPLICATION_JSON_VALUE, 
-			produces = MediaType.APPLICATION_JSON_VALUE)
+	@PostMapping(value = "/v2", consumes = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML}, 
+			produces = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML})
 	public PersonVOV2 createV2(@RequestBody PersonVOV2 person) throws Exception {
 		return service.createV2(person); 
 	}
 	
-	@PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE, 
-			produces = MediaType.APPLICATION_JSON_VALUE)
+	@PutMapping(consumes = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML}, 
+			produces = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML})
 	public PersonVO update(@RequestBody PersonVO PersonVO) throws Exception {
 		return service.update(PersonVO);
 	}
@@ -54,7 +54,7 @@ public class PersonController {
 		return ResponseEntity.noContent().build();
 	}
 	
-	@GetMapping( produces = MediaType.APPLICATION_JSON_VALUE)
+	@GetMapping( produces = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML})
 	public List<PersonVO> findAll() {
 		return service.findAll();
 	}
